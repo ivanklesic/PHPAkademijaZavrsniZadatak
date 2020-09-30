09eb248a2f42c47949cb79eb1bd6ccddf627cd49
ivanklesic <ivan.klesic@gmail.com>
2020-10-01 00:07:06 +0200

    debuging routes on server, attempt 2

--- app/Core/Routing/Router.php
+++ app/Core/Routing/Router.php
@@ -49,6 +49,7 @@ class Router implements RouterInterface
 
     public function matchRoute($path)
     {
+        str_replace('~polaznik23', '', $path);
         if(!in_array($this->request->requestMethod, $this->supportedHttpMethods))
         {
             $this->invalidMethodHandler();
