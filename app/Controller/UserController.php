a29f78823bf34a93c32a46d223c07e2c6eb5adf4
ivanklesic <ivan.klesic@gmail.com>
2020-10-01 02:01:50 +0200

    debugging routes on server, attempt 18

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -79,11 +79,11 @@ class UserController extends AbstractController
     public function registerAction()
     {
         $admin = $this->isGranted('ROLE_ADMIN');
-
+        var_dump(4);
         if ($this->session->isLoggedIn() && !$admin) {
             header('Location: /');
         }
-
+        var_dump(5);
         $this->view->render('user/register', [
             'genres' => $this->genreRepository->getList(),
             'edit' => false
