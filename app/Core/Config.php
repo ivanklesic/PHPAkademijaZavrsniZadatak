af6741e181a884f192372887cbda41c64d2c29ab
ivanklesic <ivan.klesic@gmail.com>
2020-10-01 10:46:30 +0200

    debugging routes on server, attempt 29

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -14,6 +14,6 @@ class Config
 
     public static function prependToURL()
     {
-        return $_SERVER['HTTP_HOST'] == 'phpacademy.inchoo.io' ? '/~polaznik23/' : '';
+        echo $_SERVER['HTTP_HOST'] == 'phpacademy.inchoo.io' ? '/~polaznik23/' : '';
     }
 }
